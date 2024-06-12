bb0f4166b1eb9bbfa869da481ec25034f8b3b2cc
moonyoungmin <ansdudals8@naver.com>
2024-06-13 00:21:55 +0900

    메인 화면 상단 툴바 생성

--- app/src/main/java/com/busanit/subway_project/MainActivity.kt
+++ app/src/main/java/com/busanit/subway_project/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.busanit.subway_project
 
-import android.app.AlertDialog
-import android.content.Context
+import android.app.SearchManager
 import android.graphics.Matrix
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.PopupMenu
 import android.widget.RelativeLayout
+import android.widget.Switch
+import androidx.appcompat.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.busanit.subway_project.databinding.ActivityMainBinding
+import com.github.angads25.toggle.widget.LabeledSwitch
 import com.github.chrisbanes.photoview.PhotoView
 
 class MainActivity : AppCompatActivity() {
@@ -26,12 +31,15 @@ class MainActivity : AppCompatActivity() {
         photoView = binding.photoView
         buttonsContainer = binding.buttonsContainer
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar) // 툴바를 액션바로 설정
+
         // Matrix 변화 감지 리스너 등록
         photoView.setOnMatrixChangeListener {
             updateButtonPositions()
         }
 
-        // 각 버튼에 이벤트 등록
+        // 각 역 버튼에 이벤트 등록
         val station1: Button = binding.station1
         station1.setOnClickListener {view ->
             // 역 버튼 클릭 시 이벤트 처리
@@ -48,10 +56,44 @@ class MainActivity : AppCompatActivity() {
             popupMenu.show()
         }
 
-
         // 추가 역 버튼들에 대해서도 동일하게 처리
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_search, menu)
+
+        // SearchView 설정
+        val searchItem = menu?.findItem(R.id.search)
+        val searchView = searchItem?.actionView as SearchView
+        val searchManager = getSystemService(SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        // 토글 버튼 설정
+        val toggleItem = menu.findItem(R.id.action_toggle)
+        val labeledSwitch = toggleItem.actionView?.findViewById<LabeledSwitch>(R.id.lan_switch)
+        labeledSwitch?.setOnToggledListener { labeledSwitch, isOn ->
+            // 토글 상태 변경 시 처리할 로직
+            if (isOn) {
+                photoView.setImageResource(R.drawable.busan_metro_kor)
+            }
+        }
+        toggleItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_toggle -> {
+                // 토글 버튼을 클릭했을 때 동작 구현
+                Toast.makeText(this, "클릭!", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 
     // 버튼 위치 업데이트 함수
     private fun updateButtonPositions() {
@@ -73,6 +115,7 @@ class MainActivity : AppCompatActivity() {
         button.x = scaledX
         button.y = scaledY
     }
+
 
 
 }
