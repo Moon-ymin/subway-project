90ed02cbf1514db2677acabe3547e9a56efbbe66
moonyoungmin <ansdudals8@naver.com>
2024-06-17 15:36:36 +0900

    searchview 검색 구현

--- app/src/main/java/com/busanit/subway_project/retrofit/StationAPIService.kt
+++ app/src/main/java/com/busanit/subway_project/retrofit/StationAPIService.kt
@@ -8,6 +8,9 @@ import retrofit2.http.Path
 interface StationAPIService {
     // 해당 API로 get요청을 하면 Call 객체로 반환
     @GET("/station/{scode}")    // scode로 sname 가져오기
-    fun getStationById(@Path("scode") scode: Int): Call<Station>
+    fun getStationByScode(@Path("scode") scode: Int): Call<Station>
+
+    @GET("/station/name/{sname}")   // sname으로 station 가져오기
+    fun getStationBySname(@Path("sname") sname: String): Call<Station>
 
 }
